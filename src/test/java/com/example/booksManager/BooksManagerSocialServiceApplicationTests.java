1401b0e5e6ccc16d3b46b63a29f9c7443df79770
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2023-05-06 19:44:21 +0300

    Init project

---
+++ src/test/java/com/example/booksManager/BooksManagerSocialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksManagerSocialServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
